9dadcd96f6f2d1bea9ada9b6fd135c33e1d6b70b
Arjun <arjundhav@gmail.com>
2023-01-08 16:56:54 +0530

    getter

--- Training/Training/src/com/tut/GetterSetters.java
+++ Training/Training/src/com/tut/GetterSetters.java
@@ -1,7 +1,7 @@
 package com.tut;
 
 public class GetterSetters {
-    
+
     private String name;
     private int age;
 
@@ -21,25 +21,24 @@ public class GetterSetters {
         this.age = age;
     }
 
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GetterSetters s = new GetterSetters();
         s.setAge(20);
         s.setName("Arjun");
-        System.out.println("Name: "+s.getName());
-        System.out.println("Age: "+s.getAge());
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
     }
 
-    
 }
 
 /*
-  Theory:
-  
-         Getters and Setters are used to access private variables of a class.
-         Getter returns the value , it returns the value of data type int, String, double, float, etc. 
-         For the program’s convenience, getter starts with the word “get” followed by the variable name.
-         Setter sets or updates the value. It sets the value for any variable used in a class’s programs.
-         It starts with the word “set” followed by the variable name.
 
-*/
+ Theory:
+
+ * Getters and Setters are used to access private variables of a class.
+ * Getter returns the value , it returns the value of data type int, String,double, float, etc.
+ * For the program’s convenience, getter starts with the word “get” followed by the variable name.
+ * Setter sets or updates the value. It sets the value for any variable used in a class’s programs.
+ * It starts with the word “set” followed by the variable name.
+ 
+ */
